39c4f10f5c37e38725bbd7fbe1f79d5eb8f056b7
Craig Russell <zero-degrees@users.noreply.github.com>
2017-05-10 10:51:07 -0500

    Suppress error messages when there are no children to kill

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 . cli/vars.sh
 
 #clean up child processes on exit
-trap 'kill $(jobs -p)' EXIT
+trap 'kill $(jobs -p) > /dev/null 2>&1' EXIT
 
 #start the api in the background
 supervisor -w api/app,api/server.js,config.json api/server.js &
